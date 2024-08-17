d6625bd16061bb729c5f95464131bfc85ffe91b8
josephkhan <josephzachariah.khan@gmail.com>
2024-08-17 16:20:15 +0100

    set up gamepad control for demon pov game

--- demon_pov/demon_pov_minigame.gd
+++ demon_pov/demon_pov_minigame.gd
@@ -1,8 +1,13 @@
 extends Minigame
 
 @export var demon_pov_camera : Camera3D
+@export var camera_raycast : RayCast3D
 
+@export var party_member_area_1 : Area3D
+@export var party_member_area_2 : Area3D
+@export var party_member_area_3 : Area3D
 
+@export var cow_enemy_area : Area3D
 
 func _ready() -> void:
 	pass
@@ -11,9 +16,13 @@ func _process(delta):
 	_update_input(delta)
 	
 func _update_input(delta) -> void:
-	
-	
-	pass
+	if(Input.is_action_just_pressed("minigame_button_1")):
+		var object_hit = camera_raycast.get_collider() as Node3D
+		if object_hit and object_hit.is_in_group("enemy"):
+			_on_enemy_clicked()
+		else :
+			if object_hit and object_hit.is_in_group(("party")):
+				_on_teammate_clicked()
 
 func _enter_tree() -> void:
 	pass
@@ -22,14 +31,12 @@ func _exit_tree() -> void:
 	pass
 
 func _on_button_pressed() -> void:
-	succeeded.emit()
+	pass #succeeded.emit()
 
 func _on_teammate_clicked() -> void:
-	print("FAILURE - you killed your teammate!!")
 	failed.emit()
 	
 func _on_enemy_clicked() -> void:
-	print("SUCCESS - you clicked the cow!!")
 	succeeded.emit()
 
 func _on_cow_enemy_area_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
