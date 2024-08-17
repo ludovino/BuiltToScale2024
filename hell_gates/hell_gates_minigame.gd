695f8d82371c7f4637c17b4b3e23b0494bed744d
josephkhan <josephzachariah.khan@gmail.com>
2024-08-17 21:26:44 +0100

    added progress bar to spirit align game

--- hell_gates/hell_gates_minigame.gd
+++ hell_gates/hell_gates_minigame.gd
@@ -23,8 +23,8 @@ func _updateInput(delta : float) -> void:
 	current_hell_meter = max(0, current_hell_meter)
 	if(Input.is_action_just_pressed("minigame_button_1")):
 		current_hell_meter += button_press_increment
-		print("BRUH pressed the hell button - increment hell energy")
+		#print("BRUH pressed the hell button - increment hell energy")
 		
-	print("HELLO hell value " + str(current_hell_meter))
+	#print("HELLO hell value " + str(current_hell_meter))
 	if(current_hell_meter >= button_meter_goal):
 		succeeded.emit()
--- spirit_align/spirit_align_minigame.gd
+++ spirit_align/spirit_align_minigame.gd
@@ -13,21 +13,44 @@ var seconds_passed : float
 
 @export var noise : Noise
 
+var spirit_align_seconds : float
+
+@export var goal_time : float
+@export var time_limit : float
+
+var spirits_aligned : bool
+
+@export var progress_line : TextureProgressBar
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	progress_line.max_value = goal_time
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#demon spirit movement
 	seconds_passed += delta
+	
+	#track align progress
+	if spirits_aligned:
+		spirit_align_seconds += delta
+		
+	progress_line.value = spirit_align_seconds
+		
+	if spirit_align_seconds >= goal_time:
+		succeeded.emit()
+		return
+		
+	if seconds_passed >= time_limit:
+		failed.emit()
+		return
+	#demon spirit movement
+	
 	var lerp_point = (sin(seconds_passed * demon_spirit_speed) / 2) + 0.5
 	var lerp_x = noise.get_noise_2d(seconds_passed * demon_spirit_speed, 0) + 0.5
 	var lerp_y = noise.get_noise_2d(0, seconds_passed * demon_spirit_speed)	 + 0.5
-	
-	#demon_spirit_area.position.x = lerp(demon_spirit_point_a.x, demon_spirit_point_b.x, lerp_point)
 	
 	demon_spirit_area.position.x = lerp(demon_spirit_point_a.x, demon_spirit_point_b.x, lerp_x)
 	demon_spirit_area.position.y = lerp(demon_spirit_point_a.y, demon_spirit_point_b.y, lerp_y)
@@ -35,7 +58,6 @@ func _process(delta: float) -> void:
 	#player spirit control
 	var velocity = Input.get_vector("minigame_left", "minigame_right", "minigame_up", "minigame_down") * input_sensitivity
 	
-	#player_spirit_area.global_position = Vector2(0.0, 0.0)
 	var new_player_pos = Vector2(player_spirit_area.global_position.x + velocity.x,
 	player_spirit_area.global_position.y + velocity.y)
 	
@@ -43,6 +65,14 @@ func _process(delta: float) -> void:
 	new_player_pos.y = clamp(new_player_pos.y, 0.0, 648.0)
 	
 	player_spirit_area.global_position = new_player_pos
-	
-	#print("[HELLO] demon area x pos: " + str(demon_spirit_area.position.x) + ", seconds passed " + str(seconds_passed) + " lerp point " + str(lerp_point))
-	pass
+
+
+func _on_demon_spirit_area_area_entered(area: Area2D) -> void:
+	if(area == player_spirit_area):
+		spirits_aligned = true
+		
+
+
+func _on_demon_spirit_area_area_exited(area: Area2D) -> void:
+	if(area == player_spirit_area):
+		spirits_aligned = false
