6b3cdf251d3768d9c700b7a13ac2830ad9a45ede
josephkhan <josephzachariah.khan@gmail.com>
2024-08-18 14:12:06 +0100

    started animating game over screen fade, redid victory screen name layout

--- ui/game_over_ui/game_over_ui.gd
+++ ui/game_over_ui/game_over_ui.gd
@@ -1,9 +1,11 @@
 extends Node
 
+@export var animPlayer : AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	process_mode = PROCESS_MODE_DISABLED
+	animPlayer.play("game_over_fade")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,3 +14,6 @@ func _process(delta: float) -> void:
 		get_tree().change_scene_to_file("res://battle/battle_scene.tscn")
 	if(Input.is_action_just_pressed("menu_button_2")):
 		get_tree().quit()
+
+func enable_controls() -> void:
+	process_mode = PROCESS_MODE_PAUSABLE
