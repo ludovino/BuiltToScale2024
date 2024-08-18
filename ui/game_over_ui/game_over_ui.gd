1e1269342879b1a54b0dea576f48046d37c9d099
josephkhan <josephzachariah.khan@gmail.com>
2024-08-18 14:18:55 +0100

    fixed game over fade

--- ui/game_over_ui/game_over_ui.gd
+++ ui/game_over_ui/game_over_ui.gd
@@ -2,18 +2,21 @@ extends Node
 
 @export var animPlayer : AnimationPlayer
 
+var inputDisabled : bool
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	process_mode = PROCESS_MODE_DISABLED
+	inputDisabled = true
 	animPlayer.play("game_over_fade")
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if(inputDisabled):
+		return
 	if(Input.is_action_just_pressed("menu_button_1")):
 		get_tree().change_scene_to_file("res://battle/battle_scene.tscn")
 	if(Input.is_action_just_pressed("menu_button_2")):
 		get_tree().quit()
 
 func enable_controls() -> void:
-	process_mode = PROCESS_MODE_PAUSABLE
+	inputDisabled = false
