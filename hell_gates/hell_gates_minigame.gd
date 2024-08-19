56027d6c1a205ba72e275bf411f7817f9fec7419
Olly L <otcludovino@gmail.com>
2024-08-19 17:38:32 +0100

    hell gate minigame mechanic updated

--- hell_gates/hell_gates_minigame.gd
+++ hell_gates/hell_gates_minigame.gd
@@ -7,7 +7,11 @@ extends Minigame
 var current_hell_meter : float
 
 var seconds_passed : float
+var current_size : float = 0.0
+
 @export var time_limit : float
+@export var hell_mouth : Node3D
+@export var hell_mouth_max_size : float
 
 
 # Called when the node enters the scene tree for the first time.
@@ -30,10 +34,18 @@ func _process(delta: float) -> void:
 	seconds_passed += delta
 
 	_updateInput(delta)
+	current_size = lerpf(current_hell_meter / button_meter_goal, current_size ,exp(-1.0 * delta))
+	if current_size > 0:
+		hell_mouth.visible = true
+		hell_mouth.scale = Vector3.ONE * current_size * hell_mouth_max_size
+	else:
+		hell_mouth.visible = false
 	
 	if seconds_passed >= time_limit:
 		failed.emit()
 	pass
+	
+	
 
 func _updateInput(delta : float) -> void:
 	var meter_deplete_this_frame = meter_deplete_per_second * delta
@@ -41,8 +53,5 @@ func _updateInput(delta : float) -> void:
 	current_hell_meter = max(0, current_hell_meter)
 	if(Input.is_action_just_pressed("minigame_button_1")):
 		current_hell_meter += button_press_increment
-		#print("BRUH pressed the hell button - increment hell energy")
-		
-	#print("HELLO hell value " + str(current_hell_meter))
 	if(current_hell_meter >= button_meter_goal):
 		succeeded.emit()
