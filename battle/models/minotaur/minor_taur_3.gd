e28af97ef6e506769a58b94b8af63ee15b003f9a
vivivi <viv@trashcompactor.co.uk>
2024-08-20 13:06:49 +0100

    camera animation updates and added minotaur possess animation

--- battle/models/minotaur/minor_taur_3.gd
+++ battle/models/minotaur/minor_taur_3.gd
@@ -4,6 +4,13 @@ func climb():
 	visible = true
 	$AnimationPlayer.play("ClimbFromHell")
 	$AnimationPlayer.queue("Idle")
+	
+func butterflyplay():
+	$AnimationPlayer.play("ButterflyGame")
+	
+func butterflywin():
+	$AnimationPlayer.play("ButterflyGameWin")
+	$AnimationPlayer.queue("Idle")
 
 func idle():
 	$AnimationPlayer.play("Idle")
